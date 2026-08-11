2ecc80df647cbb22f51adc6e97dcb308e0b5cfa1
MrNotFound <rustamhydyrov485@gmail.com>
2026-08-11 11:28:34 +0500

    Prepare package for PyPI

--- src/percent_choice/percent.py
+++ src/percent_choice/percent.py
@@ -1,40 +1,32 @@
-from random import randrange
+from random import random
 from bisect import bisect
 from secrets import randbelow
 
 class Percent:
-    __slots__ = ("_objects","_cumulative",)
+    __slots__ = ("_objects", "_cumulative")
+
     def __init__(self, objects, chances):
         if len(objects) != len(chances):
-            raise ValueError(
-                "objects and chances "
-                "must have the same length"
-            )
+            raise ValueError("objects and chances must have the same length")
         if not objects:
-            raise ValueError(
-                "objects cannot be empty"
-            )
-        if not all(isinstance(x, int) for x in chances) or any(x < 0 for x in chances):
-            raise TypeError(
-                "all values in chances must be int or values in chances cannot be negative"
-            )
+            raise ValueError("objects cannot be empty")
+        if not all(isinstance(x, (int, float)) for x in chances):
+            raise TypeError("all values in chances must be int or float")
+        if any(x < 0 for x in chances):
+            raise ValueError("chances cannot be negative")
         if sum(chances) != 100:
-            raise ValueError(
-                "sum of chances must be 100"
-            )
-
+            raise ValueError("sum of chances must be 100")
         self._objects = tuple(objects)
         cumulative = []
-        total = 0
+        total = 0.0
         for chance in chances:
             total += chance
             cumulative.append(total)
         self._cumulative = tuple(cumulative)
     def get(self):
-        number = randrange(100)
+        number = random() * 100
         index = bisect(self._cumulative, number)
         return self._objects[index]
-
 class SecurePercent:
     __slots__ = ("_objects","_cumulative",)
     def __init__(self, objects, chances):
